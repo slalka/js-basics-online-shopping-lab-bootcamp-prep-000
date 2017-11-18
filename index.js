a733f40f242f24133fb7113639661d575bcbbda7
stephan lalka <slalka@gmail.com>
2017-11-18 22:38:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ cart.push({[item]:~~(Math.random()*100)})
+ console.log(`${item} has been added to your cart.`)
+ return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if(!cart.length) return console.log("Your shopping cart is empty.")
+  var a = "In your cart, you have "
+
+  for(var i = 0; i<cart.length;i++){
+    if((i+1 === cart.length) && (i>0) ){
+      a = a + "and "
+    }
+
+     var item = Object.keys(cart[i])[0]
+     var s = item + " at $" + cart[i][item]
+    a = a + s
+  }
+  a = a + "."
+  return console.log(a)
+
+
 }
 
 function total() {
