427cddbab680d33cb4864a759d350800f205cdb4
stephan lalka <slalka@gmail.com>
2017-11-19 01:19:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function total() {
 
 function removeFromCart(item) {
   for(var i in cart){
-    if(cart[i][item]) delete cart[i].item; return;
+    if(cart[i][item]) delete cart[i][item]; return;
   }
   console.log('That item is not in your cart.')
   // write your code here
